bcefd1dc94162da1990af322678c05b045ce61cd
Pacito2 <you@example.com>
2022-04-01 11:57:55 +0200

    Fixing routing (temp removing isLighting and isAlarming from tab1)

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -29,7 +29,7 @@ export class Tab1Page {
   }
 
   isLighting(): boolean{
-    return this.objectsService.objects.spotlight;
+    return true;//this.objectsService.objects.spotlight;
   }
 
   async toggleAlarm(){
@@ -37,6 +37,6 @@ export class Tab1Page {
   }
 
   isAlarming(): boolean{
-    return this.objectsService.objects.alarm;
+    return true;//this.objectsService.objects.alarm;
   }
 }
