78f4ce0904be9d7c7ccb5e9fc1e9e77081c9ed92
Pacito2 <you@example.com>
2022-05-10 14:34:48 +0200

    Editing stream layou

--- src/app/pages/tab1/tab1.module.ts
+++ src/app/pages/tab1/tab1.module.ts
@@ -7,9 +7,9 @@ import { Tab1PageRoutingModule } from './tab1-routing.module';
 import { SwiperModule } from 'swiper/angular';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
-import { BrowserModule } from '@angular/platform-browser';
+import { environment } from 'src/environments/environment';
 
-const config: SocketIoConfig = { url:  `http://192.168.43.254:3000`,  options: {}};
+const config: SocketIoConfig = { url:  `http://${ environment.serverIp }:${ environment.port }`,  options: {}};
 
 @NgModule({
   imports: [
